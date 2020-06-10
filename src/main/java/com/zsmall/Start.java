51eb7cf126aaccc4950e5b87143f408c5109f77a
zs199582 <1181262579@qq.com>
2020-06-10 23:18:18 +0800

    test

--- src/main/java/com/zsmall/Start.java
+++ src/main/java/com/zsmall/Start.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @SpringBootApplication(scanBasePackages = "com.zsmall")
 @RestController
 @MapperScan("com.zsmall.dao")
@@ -15,8 +18,17 @@ public class Start {
         SpringApplication.run(Start.class,args);
 //        int a = Math.abs(String.valueOf(300204464).hashCode()%1009);
 //        System.out.println(a);
-        long couponId = Long.parseLong("21992707");
-        System.out.println(couponId);
-        String methodName = Thread.currentThread().getStackTrace()[0].getMethodName();
+//        long couponId = Long.parseLong("21992707");
+//        System.out.println(couponId);
+//        String methodName = Thread.currentThread().getStackTrace()[0].getMethodName();
+//        Map<String,Boolean> testMap = new HashMap<>();
+//        testMap.put("1",true);
+//        testMap.put("2",false);
+//        testMap.put("3",false);
+//        boolean result = true;
+//        for (String couponCode : testMap.keySet()) {
+//            result = result && testMap.get(couponCode);
+//        }
+        System.out.println(Integer.parseInt(null));
     }
 }
